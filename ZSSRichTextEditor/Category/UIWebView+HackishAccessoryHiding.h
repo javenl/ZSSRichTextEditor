6437fc765e4da9cd7cb8d930c8bba9ee1403932f
Javen_liu <412775083@qq.com>
2015-06-23 10:54:48 +0800

    add

---
+++ ZSSRichTextEditor/Category/UIWebView+HackishAccessoryHiding.h
@@ -0,0 +1,15 @@
+//
+//  UIWebView+Hide.h
+//  ZSSRichTextEditor
+//
+//  Created by liu on 15-6-23.
+//  Copyright (c) 2015年 Zed Said Studio. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIWebView (HackishAccessoryHiding)
+
+@property (nonatomic, assign) BOOL hidesInputAccessoryView;
+
+@end
